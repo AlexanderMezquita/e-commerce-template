e965e5aa4e02373e4680f53103f82262da5a9587
Alex <alexmezsan@gmail.com>
2023-05-28 18:05:29 -0400

    landing videos and images added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,66 @@ export default function Home() {
     <>
       <main>
         <Offer />
-        <div id="page" className=" max-w-screen-2xl mx-auto">
-          <div>Hello</div>
+        <div id="page" className=" max-w-screen-2xl mx-auto space-y-5">
+          <section className="relative mt-5 ">
+            <picture className=" hidden md:block">
+              {/* <source media="(max-width:750px)" srcSet="/assets/landing.webp" /> */}
+              <source
+                media="(max-width:765px)"
+                srcSet="/assets/landing-bg.-sm.jpg"
+              />
+              <img
+                alt="landing image"
+                src="/assets/landing.webp"
+                className=" object-center "
+              />
+            </picture>
+            <video
+              loop
+              muted
+              autoPlay
+              preload="auto"
+              playsInline
+              className=" block md:hidden"
+            >
+              <source src="assets/landing-video-sm.mp4" type="video/mp4" />
+            </video>
+
+            <div className="absolute top-0 flex flex-col justify-center items-center md:items-start w-full h-full">
+              <div className=" flex flex-col items-center w-6/12 gap-5">
+                <h1 className=" text-2xl text-white uppercase">trend edit</h1>
+                <h1 className=" text-5xl text-white uppercase">
+                  Noticeably 90s
+                </h1>
+                <ButtonTrans link="shop women's" />
+              </div>
+            </div>
+          </section>
+
           <section className="relative ">
-            <img
-              alt="landing image"
-              src="/assets/landing-bg.jpg"
-              className=" object-fill max-h-[850px] w-full "
-            />
-            <div className="absolute top-0 flex flex-col justify-center items-end w-full h-full px-20">
-              <div className=" flex flex-col items-center gap-5">
-                <h1 className=" text-5xl text-white">The Essential Tee</h1>
+            <picture>
+              {/* <source media="(max-width:750px)" srcSet="/assets/landing.webp" /> */}
+              <source
+                media="(max-width:765px)"
+                srcSet="/assets/landing-bg.-sm.jpg"
+              />
+              <img
+                alt="landing image"
+                src="/assets/landing-bg.jpg"
+                className=" object-center "
+              />
+            </picture>
+
+            <div className="absolute top-0 flex flex-col justify-center items-center md:items-end w-full h-full">
+              <div className=" flex flex-col items-center w-6/12 gap-5">
+                <img
+                  alt="Get away shop"
+                  src="/assets/GETAWAYSHOP2022.svg"
+                  className=" w-28 md:w-32"
+                />
+                <h1 className=" text-3xl md:text-5xl text-white">
+                  The Essential Tee
+                </h1>
                 <ButtonTrans link="shop men's" />
               </div>
             </div>
--- src/components/navbar/offer.tsx
+++ src/components/navbar/offer.tsx
@@ -1,17 +1,17 @@
 export default function Offer() {
   return (
-    <article className=" w-full bg-primary min-[48px]:py-3 text-white">
+    <article className=" w-full bg-primary min-[48px]:py-3 text-white text-xs">
       <div className=" max-w-screen-2xl px-4 mx-auto flex flex-col lg:flex-row lg:justify-start justify-center items-center lg:space-x-10">
         <p>Limited Time: Up to 25% Off select styles</p>
-        <ul className="lg:flex lg:space-x-5 hidden [&>li]:flex [&>li]:justify-center [&>li]:items-center ">
+        <ul className="lg:flex lg:space-x-5 hidden [&>li]:flex [&>li]:justify-center [&>li]:items-center [&>li>a]:underline [&>li>a]:font-light ">
           <li>
             <a>See Details</a>
           </li>
           <li>
-            <a>See Details</a>
+            <a>Shop Men's</a>
           </li>
           <li>
-            <a>See Details</a>
+            <a>Shop Women's</a>
           </li>
         </ul>
       </div>
