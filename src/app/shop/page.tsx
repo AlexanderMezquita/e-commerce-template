633f07e9b321de18a30546adc592d5dc15233e6c
Alam <amenabeato@gmail.com>
2023-06-02 23:43:27 -0400

    products page init

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,3 +1,25 @@
-export default async function Page() {
-  <>Page shop</>;
+import Offer from "@/components/navbar/offer";
+import Subcategories from "@/components/shop/subcategories";
+import { GetProducts } from "./actions";
+
+export default async function Shop() {
+  const products = await GetProducts();
+
+  return (
+    <main>
+      <Offer />
+      <div className="main-container grid grid-cols-12">
+        <aside className="md:block md:col-span-4 hidden">
+          {/* @ts-expect-error Server Component */}
+          <Subcategories category="d527da3e-a843-4df9-8017-8de53842a6d6" />
+        </aside>
+
+        <section>
+          {products.map((product) => (
+            <>{product.name}</>
+          ))}
+        </section>
+      </div>
+    </main>
+  );
 }
